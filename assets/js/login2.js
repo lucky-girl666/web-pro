72c533853184f53b6717a423397f456a50a5bd24
liurui <1436937168@qq.com>
2023-03-22 00:06:37 +0800

    完成了登录和注册开发

--- assets/js/login2.js
+++ assets/js/login2.js
@@ -21,4 +21,39 @@ $(function () {
         }
     })
 
+    // 监听注册表单事件
+    $('#form-td').on('submit',function (e) {
+        e.preventDefault()
+        $.post('/api/reguser',
+        {
+            username: $('#form-td [name = username]').val(),
+            password: $('#form-td [name = password]').val()
+        },
+        function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登录')
+            $('#link_login').click();
+        })
+    })
+
+    // 监听登录表单事件
+    $('#form-fb').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    layer.msg(res.message)
+                }
+                layer.msg('登录成功')
+                localStorage.setItem('token', res.token);
+                location.href = '/index.html';
+            }
+        })
+    })
+
 })
